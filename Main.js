2cf161addede4149e601f8048fd395ba11013b07
Laz-k-dev <kimani.lazarus@strathmore.edu>
2026-06-24 00:01:45 +0300

    Added some images in the home page

---
+++ Main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("contactForm");
+
+    if(form) {
+        form.addEventListener("submit", function(event) {
+            let name = document.getElementById("name").value.trim();
+            let email = document.getElementById("email").value.trim();
+            let message = document.getElementById("message").value.trim();
+
+            if(name === "" || email === "" || message === "") {
+                alert("Please fill in all fields.");
+                event.preventDefault();
+            }
+        });
+    }
+});
+
+const searchInput = document.getElementById("searchInput");
+if (searchInput) {
+    searchInput.addEventListener("keyup", function() {
+        let filter = searchInput.value.toLowerCase();
+        let cards = document.querySelectorAll(".product-card");
+
+        cards.forEach(card => {
+            let title = card.textContent.toLowerCase();
+            
+            if(text.includes(filter)) {
+                card.style.display = "";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    });
+}
